9e53930729d3e2a7860cf8940f69f27538e31857
GameZone\Vinothkrishna <vinothkrishna3@gmail.com>
2017-04-09 08:54:14 +0530

    First commit

---
+++ src/com/bean/Address.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package com.bean;
+
+/**
+ * @author Vinothkrishna
+ *
+ */
+
+public class Address {
+	
+	private String line1;
+	private String line2;
+	private String state;
+	private String city;
+	private long pincode;
+	public String getLine1() {
+		return line1;
+	}
+	public void setLine1(String line1) {
+		this.line1 = line1;
+	}
+	public String getLine2() {
+		return line2;
+	}
+	public void setLine2(String line2) {
+		this.line2 = line2;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public long getPincode() {
+		return pincode;
+	}
+	public void setPincode(long pincode) {
+		this.pincode = pincode;
+	}
+
+}
---
+++ src/com/bean/Employee.java
@@ -0,0 +1,60 @@
+/**
+ * 
+ */
+package com.bean;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * @author Vinothkrishna
+ *
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement(name = "employee")
+public class Employee {
+	
+	@XmlAttribute
+	private int id;
+
+	private String name;
+	private String designation;
+	private String role;
+	
+	private Address address;
+
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDesignation() {
+		return designation;
+	}
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+}
